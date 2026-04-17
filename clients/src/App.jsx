c6f17eb22472b2cac9c8cf8c623824a436f34d41
Pindi-Srilasya <pindisrilasya2006@gmail.com>
2026-04-17 14:46:56 +0530

    products added to Cart

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -6,6 +6,9 @@ import Login from './components/Login'
 import Addproduct from './components/Addproduct'
 import Home from './components/Home'
 import ProtectedRoute from './components/ProtectedRoute'
+import Cart from './components/Cart'
+
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -17,6 +20,11 @@ export default function App() {
         <Route path="/add-product" element={
           <ProtectedRoute>
             <Addproduct/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/cart" element={
+          <ProtectedRoute>
+            <Cart/>
           </ProtectedRoute>
         }/>
 
